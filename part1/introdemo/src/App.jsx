3d7394cf392ab6eeecfbfae8448d8761508a3602
Tynan Schweitzer <tynanschweitzer@pm.me>
2025-02-06 20:51:44 +0800

    FEAT: Part 1-d Complete

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,37 +1,25 @@
 import { useState } from "react";
 
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  };
+  const [value, setValue] = useState(10);
 
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
-};
-
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
 };
 
 export default App;
